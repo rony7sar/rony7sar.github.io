d22591a2f3a4b35d77216c9516a2eab8bad45aac
rony7sar <rony7sar@users.noreply.github.com>
2018-01-21 21:02:24 +0530

    rony7sar upload

---
+++ dropDown.php
@@ -0,0 +1,15 @@
+<?php
+	$mca = array("sem1"=>"I","sem2"=>"II","sem3"=>"II","sem4"=>"IV","sem5"=>"V","sem6"=>"VI",);
+	$mcom = array("sem1"=>"I","sem2"=>"II","sem3"=>"II","sem4"=>"IV");
+	$mba = array("sem1"=>"I","sem2"=>"II","sem3"=>"II","sem4"=>"IV");
+	
+	//$q = $_REQUEST["q"];
+	$q = "mca";
+	//echo "<select>";
+	//foreach($$q as $key => $value){
+	//	echo "<option value=".$key.">".$value."</option>";
+	
+	//}
+	//echo "</select>";
+	echo "Hello!!!";
+?>
